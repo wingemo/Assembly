8f8ec52c28ba3243270c9e3bb6da6b034d78f093
Wingemo <philip.wingemo@outlook.com>
2021-04-13 23:43:27 +0200

    Update Jtag.s

--- Jtag.s
+++ Jtag.s
@@ -2,13 +2,13 @@ global _start
 _start:
 
 .macro PUSH reg
-subi sp, sp, 4  /* reserve space on the stack */
-stw \reg, 0(sp) /* save register */
+  subi sp, sp, 4  /* reserve space on the stack */
+  stw \reg, 0(sp) /* save register */
 .endm
 
 .macro POP reg
-ldw r4, 0(sp) /* restore registers */
-addi sp, sp, 4 
+  ldw r4, 0(sp) /* restore registers */
+  addi sp, sp, 4 
 .endm
 
 /* set up stack pointer */
@@ -19,32 +19,32 @@ movia r6, 0x10001000 /* JTAG UART base address */
 movia r8, TEXT_STRING
 
 MAIN_LOOP:
-ldb r5, 0(r8)
-beq r5, zero, GET_JTAG 
-call PUT_JTAG
-addi r8, r8, 1
-br MAIN_LOOP
+  ldb r5, 0(r8)
+  beq r5, zero, GET_JTAG 
+  call PUT_JTAG
+  addi r8, r8, 1
+  br MAIN_LOOP
 
 GET_JTAG:
-ldwio r4, 0(r6) /* read the JTAG UART Data register */
-andi r8, r4, 0x8000  /* check if there is new data */
-beq r8, r0, GET_JTAG  /* if no data, wait */
-andi r5, r4, 0x00ff /* the data is in the least significant byte */
-PUSH r31 /* save away the return address */
-call PUT_JTAG /* echo character */
-POP r31 /* restore return address */
-br GET_JTAG /* Loop */
+  ldwio r4, 0(r6) /* read the JTAG UART Data register */
+  andi r8, r4, 0x8000  /* check if there is new data */
+  beq r8, r0, GET_JTAG  /* if no data, wait */
+  andi r5, r4, 0x00ff /* the data is in the least significant byte */
+  PUSH r31 /* save away the return address */
+  call PUT_JTAG /* echo character */
+  POP r31 /* restore return address */
+  br GET_JTAG /* Loop */
 
 PUT_JTAG:
-PUSH r5 /* reserve space on the stack */
-ldwio r4, 4(r6) /* read the JTAG UART Control register */
-andhi r4, r4, 0xffff  /* check for write space */	
-beq r4, r0, END_PUT /* if no space, ignore the character */
-stwio r5, 0(r6)  /* send the character */
+  PUSH r5 /* reserve space on the stack */
+  ldwio r4, 4(r6) /* read the JTAG UART Control register */
+  andhi r4, r4, 0xffff  /* check for write space */	
+  beq r4, r0, END_PUT /* if no space, ignore the character */
+  stwio r5, 0(r6)  /* send the character */
 
 END_PUT:
-POP r5 /* restore registers */
-ret
+  POP r5 /* restore registers */
+  ret
 
 
 TEXT_STRING:

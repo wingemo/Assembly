6e6f073ca2daf801f22b73d5a9c39a1c6cb5caf2
Wingemo <philip.wingemo@outlook.com>
2021-04-15 22:05:09 +0200

    Update LED.s

--- LED.s
+++ LED.s
@@ -5,20 +5,20 @@ _start:
 # These addresses needs to be set to the correct values
 	movia	r17,0x10000000	# Address for the red LEDs
 	movia	r18,0x10000010	# Address for the green LEDs
-	movia  r10,0x4
-	movia  r12,0x1
+	movia   r10,0x4
+	movia   r12,0x1
 	movia	r9, 0xaaaaaaaa
 	
 # This segment writes to the red LEDs, waits
 # for the timer and then turns the red LEDs off.
 RED: 
-	srl        r9, r9, r12
+	srl     r9, r9, r12
         mov     r8, r9
 	stwio	r8,0(r17)
 	call	TIMER
 	stwio	r0,0(r17)
-	call      COUNTDOWN
-	br        RED
+	call    COUNTDOWN
+	br      RED
 
 # This segment writes to the green LEDs, waits
 # for the timer and then turns the green LEDs off.
@@ -30,12 +30,12 @@ GREEN:
 	br      END
 
 COUNTDOWN:
-	subi  r10,r10,0x1
-	beq   r10,r0, GREEN  
+	subi    r10,r10,0x1
+	beq     r10,r0, GREEN  
 	ret 
 	
 TIMER:
-	movia	r8, 0x989757 # Is this value reasonable? 0xffffff
+	movia	r8, 0x989757 
 	
 START_TIMER:
 	subi	r8,r8,0x1

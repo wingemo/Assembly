6142ea88d2a0a8ad0ff5124ce6b635f9408ad2fc
Wingemo <philip.wingemo@outlook.com>
2021-07-23 19:03:24 +0200

    Update Switches.s

--- Switches/Switches.s
+++ Switches/Switches.s
@@ -1,13 +1,13 @@
 .text
 .global _start
-_start:
-	 movia r6,  0x10000040 /* Switches base address */
-	 movia r18, 0x10000010 /* LEDs base address */
+_start: 
+  movia r6,  0x10000040 /* Switches base address */
+  movia r18, 0x10000010 /* LEDs base address */
 	 
 MAIN_LOOP:	 
-	 ldwio   r4,  0(r6)
-	 stwio	 r4,  0(r18)
-	 br      MAIN_LOOP
+  ldwio r4,  0(r6)
+  stwio	r4,  0(r18)
+  br    MAIN_LOOP
 
 END:
-         .end
+  .end

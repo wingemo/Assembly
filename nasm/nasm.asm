5de604b0c78f9784c7d317a0583018011c2ccfeb
Wingemo <philip.wingemo@outlook.com>
2021-09-24 23:19:17 +0200

    Update nasm.asm

--- nasm/nasm.asm
+++ nasm/nasm.asm
@@ -6,20 +6,28 @@ section .bss
 section .text   
 _start:
 
+    MAIN_LOOP:
+    push    ecx           ; save away the return address
+
     ; System call (sys_read) 
-    mov     edx, 1      ; number of bytes to read
-    mov     ecx, input  ; reserved space to store our input (known as a buffer)
-    mov     ebx, 0      ; write to the STDIN file
-    mov     eax, 3      ; invoke SYS_READ (kernel opcode 3)
+    mov     edx, 1        ; number of bytes to read
+    mov     ecx, input    ; reserved space to store our input (known as a buffer)
+    mov     ebx, 0        ; write to the STDIN file
+    mov     eax, 3        ; invoke SYS_READ (kernel opcode 3)
     int     80h
 
+
     ; System call (sys_write) 
-    mov     ecx, input ; Store memory address
+    mov     ecx, input  ; Store arguments to the system cal,  move the memory address of sum into ecx
     mov     edx, 1     ; number of bytes to write 
-    mov     ebx, 1     ; write to the STDOUT file
-    mov     eax, 4     ; system call number 
+    mov     ebx, 1     ; Store arguments to the system cal, write to the STDOUT file
+    mov     eax, 4     ; system call number (sys_write) 
     int     0x80       ; call kernel
     
+    pop     ecx        ; restore return address
+    loop    MAIN_LOOP  
+    
     ; System call (sys_exit)
-    mov     eax, 1     ; system call number 
+    mov     eax, 1     ; system call number (sys_exit) 
     int     0x80       ; call kernel
+    

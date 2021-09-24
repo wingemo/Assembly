f844be2c108c8d248f2abd9642d09973c6e45a03
Wingemo <philip.wingemo@outlook.com>
2021-09-25 00:58:06 +0200

    Update echo.asm

--- x86 CPU architecture/echo.asm
+++ x86 CPU architecture/echo.asm
@@ -1,5 +1,7 @@
 global _start
 
+
+    file db 'data.txt', 0h    
 section .bss
     input: resb 4   
     
@@ -16,15 +18,21 @@ _start:
     mov     eax, 3        ; invoke SYS_READ (kernel opcode 3)
     int     80h
 
+    mov     edx, 12       ; number of bytes to write - one for each letter of our contents string
+    mov     ecx, input    ; move the memory address of our contents string into ecx
+    mov     ebx, file     ; move the file descriptor of the file we created into ebx
+    mov     eax, 4        ; invoke SYS_WRITE (kernel opcode 4)
+    int     80h 
+
+    pop     ecx           ; restore return address
+    loop    MAIN_LOOP  
+
     ; System call (sys_write) 
     mov     ecx, input    ; Store arguments to the system cal,  move the memory address of sum into ecx
     mov     edx, 1        ; number of bytes to write 
     mov     ebx, 1        ; Store arguments to the system cal, write to the STDOUT file
     mov     eax, 4        ; system call number (sys_write) 
     int     0x80          ; call kernel
-    
-    pop     ecx           ; restore return address
-    loop    MAIN_LOOP  
     
     ; System call (sys_exit)
     mov     eax, 1       ; system call number (sys_exit) 

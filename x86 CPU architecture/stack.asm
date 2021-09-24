cab4509d9d399e77632b2b631dcbde81fc9a54f1
Wingemo <philip.wingemo@outlook.com>
2021-09-25 01:43:38 +0200

    Update stack.asm

--- x86 CPU architecture/stack.asm
+++ x86 CPU architecture/stack.asm
@@ -11,18 +11,19 @@ _start:
     INPUT_LOOP:
 
     ; System call (sys_read) 
-    mov     edx, 10       ; number of bytes to read
-    mov     ecx, input    ; reserved space to store our input (known as a buffer)
-    mov     ebx, 0        ; write to the STDIN file
-    mov     eax, 3        ; invoke SYS_READ (kernel opcode 3)
+    mov     edx, 10              ; number of bytes to read
+    mov     ecx, input           ; reserved space to store our input (known as a buffer)
+    mov     ebx, 0               ; write to the STDIN file
+    mov     eax, 3               ; invoke SYS_READ (kernel opcode 3)
     int     0x80
- 
-    push    ecx           ; save away the return address
-    
+
+    ; System call (sys_read) 
+    movzx   eax, byte [input]    ; start with the first digit
+    sub     eax, '0'
+    push    eax                  ; save away the return address
     loop    INPUT_LOOP  
     
     ; System call (sys_exit)
     mov     eax, 1       ; system call number (sys_exit) 
     int     0x80         ; call kernel
-   
-
+  

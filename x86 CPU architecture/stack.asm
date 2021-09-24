8b03d52135e2f70cad6e2431f05ca6f76809e4fb
Wingemo <philip.wingemo@outlook.com>
2021-09-25 01:55:34 +0200

    Update stack.asm

--- x86 CPU architecture/stack.asm
+++ x86 CPU architecture/stack.asm
@@ -29,6 +29,14 @@ _start:
     add     sum, ebx
 
     loop    MATH_LOOP 
+    
+    ; System call (sys_write) 
+    mov     ecx, input           ; Store arguments to the system cal,  move the memory address of sum into ecx
+    mov     edx, 1               ; number of bytes to write 
+    mov     ebx, 1               ; Store arguments to the system cal, write to the STDOUT file
+    mov     eax, 4               ; system call number (sys_write) 
+    int     0x80                 ; call kernel
+
 
     mov     eax, 1               ; system call number (sys_exit) 
     int     0x80                 ; call kernel

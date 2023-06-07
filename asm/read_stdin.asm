bdbc580f04cb0951fdbacbf88b773ca855a63bf1
Wingemo <philip.wingemo@outlook.com>
2023-06-07 13:09:53 +0200

    Update read_stdin.asm

--- asm/read_stdin.asm
+++ asm/read_stdin.asm
@@ -1,14 +1,14 @@
+; Read from stdin (standard input)
+
 section .text
     global read_input
 
 read_stdin:
-    ; Read from stdin (standard input)
     mov rax, 0        ; sys_read
     mov rdi, 0        ; stdin file descriptor
     mov rdx, [rsi]    ; Load buffer_size from the parameter
     syscall           ; Make the system call
 
-    ; Save the stdin content to a register (e.g., r8)
-    mov r8, rax
+    mov r8, rax       ; Save the stdin content (e.g., r8)
 
     ret

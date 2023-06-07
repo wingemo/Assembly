457cb310fd5841d91fcee8fa6ed2ec9ca8de1388
Wingemo <philip.wingemo@outlook.com>
2023-06-07 12:59:43 +0200

    Update main.asm

--- asm/main.asm
+++ asm/main.asm
@@ -4,15 +4,15 @@ section .data
     buffer_size equ 64
 
 section .text
-    extern read_and_display
+    extern read_stdin
     extern exit_program
     global _start
 
 _start:
-    ; Call the read_and_display subroutine
+    ; Call the read_stdin subroutine
     mov rdi, buffer       ; Pass buffer as the parameter
     mov rsi, buffer_size  ; Pass buffer_size as the parameter
-    call read_and_display
+    call read_stdin
 
     ; Call the exit_program subroutine
     call exit_program

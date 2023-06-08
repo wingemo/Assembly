7b370f1fe82ad21ad3860c76cfa56a84e0f37ed1
Wingemo <philip.wingemo@outlook.com>
2023-06-08 12:39:45 +0200

    Update sub_stdin.asm

--- asm/sub_stdin.asm
+++ asm/sub_stdin.asm
@@ -6,15 +6,18 @@
 ; Return Value: 
 ;   - rax: Content of r8 register (stdin content)
 ;--------------------------------------------------------------
+section .data
+    buffer resb 64        ; Buffer for input
+
 section .text
     global read_stdin
 
 read_stdin:
-    mov rax, 0        ; sys_read
-    mov rdi, 0        ; stdin file descriptor
-    mov rdx, [rsi]    ; Load buffer_size from the parameter
-    syscall           ; Make the system call
+    mov rax, 0           ; sys_read
+    mov rdi, 0           ; stdin file descriptor
+    mov rdx, [buffer]    ; Load buffer_size from the parameter
+    syscall              ; Make the system call
 
-    mov r8, rax       ; Save the stdin content (e.g., r8)
+    mov r8, rax          ; Save the stdin content (e.g., r8)
 
     ret

6fb0f6c5e5cfcdcbfd386f8327d99a481d7d5ac3
Wingemo <philip.wingemo@outlook.com>
2023-06-08 12:20:03 +0200

    Update exit.asm

--- asm/exit.asm
+++ asm/exit.asm
@@ -8,7 +8,6 @@ section .text
     global exit_program
 
 exit_program:
-    ; Exit the program
     mov rax, 60       ; sys_exit
     xor rdi, rdi      ; exit status 0
     syscall           ; make the system call

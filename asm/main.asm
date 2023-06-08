612b088da212aa4a6464406a1db896de01e15678
Wingemo <philip.wingemo@outlook.com>
2023-06-08 12:31:22 +0200

    Update main.asm

--- asm/main.asm
+++ asm/main.asm
@@ -1,23 +1,20 @@
 section .data
-    buffer  resb 64                   ; Buffer for input
+    buffer resb 64                   ; Buffer for input
 
 section .text
     extern subroutine_stdin
     extern subroutine_terminate
+    extern setup_stack
     global _start
 
-section .bss
-    stack_size  equ 1024
-    stack       resb stack_size      ; Stack space
-
 _start:
-    mov  rsp, stack                  ; Set stack pointer to the start of the stack
+    call setup_stack               ; Call the setup_stack subroutine
 
     mov  rdi, buffer                 ; Pass buffer as the parameter
     call subroutine_stdin            ; Call the read_stdin subroutine
     mov  byte [rsp - 1], r8          ; Push the value r8 onto the stack
 
     call subroutine_terminate        ; Call the exit_program subroutine
-    
+
 
 

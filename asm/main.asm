42bac33c30723d4d1c37097949e6a83bae56d049
Wingemo <philip.wingemo@outlook.com>
2023-06-08 12:38:14 +0200

    Update main.asm

--- asm/main.asm
+++ asm/main.asm
@@ -2,19 +2,19 @@ section .data
     buffer resb 64                   ; Buffer for input
 
 section .text
-    extern subroutine_stdin
-    extern subroutine_terminate
-    extern setup_stack
+    extern sub_stdin
+    extern sub_terminate
+    extern sub_setup_stack
     global _start
 
 _start:
-    call setup_stack                 ; Call the setup_stack subroutine
+    call sub_setup_stack             ; Call the setup_stack subroutine
 
     mov  rdi, buffer                 ; Pass buffer as the parameter
-    call subroutine_stdin            ; Call the read_stdin subroutine
+    call sub_stdin                   ; Call the read_stdin subroutine
     mov  byte [rsp - 1], r8          ; Push the value r8 onto the stack
 
-    call subroutine_terminate        ; Call the exit_program subroutine
+    call sub_terminate               ; Call the exit_program subroutine
 
 
 

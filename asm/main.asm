81ba96524a0afe4ec57665898a3adcb01c80e193
Wingemo <philip.wingemo@outlook.com>
2023-06-08 12:01:21 +0200

    Update main.asm

--- asm/main.asm
+++ asm/main.asm
@@ -1,13 +1,13 @@
 section .data
-    buffer resb 64         ; Buffer for input
+    buffer resb 64           ; Buffer for input
 
 section .text
     extern read_stdin
     extern exit_program
     global _start
 
-_start:
-    mov rdi, buffer       ; Pass buffer as the parameter
-    call read_stdin       ; Call the read_stdin subroutine
+_start: 
+    mov    rdi, buffer      ; Pass buffer as the parameter
+    call   read_stdin       ; Call the read_stdin subroutine
 
-    call exit_program     ; Call the exit_program subroutine
+    call   exit_program     ; Call the exit_program subroutine
